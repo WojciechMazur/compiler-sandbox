5d0ed428281b88c44286130827318ce206886acd
Wojciech Mazur <wmazur@virtuslab.com>
2024-09-13 11:22:22 +0200

    Initial commit + Test spire

---
+++ test.scala
@@ -0,0 +1,11 @@
+//> using dep "org.typelevel::spire:0.18.0"
+//> using options -language:strictEquality -Yexplicit-nulls -Yno-flexible-types 
+//> using options -Wsafe-init -explain -indent  -new-syntax -Xfatal-warnings -Wunused:all -Wvalue-discard -Wnonunit-statement
+//> using options -Xcheck-macros -source:future -Ydebug-unpickling 
+
+import spire.math.UInt
+import spire.implicits.*
+
+class TestingCode:
+  def testFunction(value: UInt): Unit = ???
+  val tc = TestingCode().testFunction(ui"1")
