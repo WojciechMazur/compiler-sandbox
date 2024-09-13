afcba8024cacf4159c2a892d7f37046efa5fb989
Wojciech Mazur <wmazur@virtuslab.com>
2024-09-13 16:05:25 +0200

    Test multline string match on Windows

--- test.scala
+++ test.scala
@@ -1,11 +1,10 @@
-//> using dep "org.typelevel::spire:0.18.0"
-//> using options -language:strictEquality -Yexplicit-nulls -Yno-flexible-types 
-//> using options -Wsafe-init -explain -indent  -new-syntax -Xfatal-warnings -Wunused:all -Wvalue-discard -Wnonunit-statement
-//> using options -Xcheck-macros -source:future -Ydebug-unpickling 
-
-import spire.math.UInt
-import spire.implicits.*
-
-class TestingCode:
-  def testFunction(value: UInt): Unit = ???
-  val tc = TestingCode().testFunction(1)
+val msg = s"""{"event": "foo",
+             |, "ts": 123456
+             |}""".stripMargin.replaceAll(System.lineSeparator(), "\n")
+@main def Test = msg match {
+  case s"""{"event": "$event",
+, "ts": $ts
+}""" =>
+    assert(event == "foo")
+    assert(ts.toInt == 123456)
+}
