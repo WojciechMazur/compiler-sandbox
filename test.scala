a6e9b22f917a5e0b890845aa2e151c9ed3db70ad
Wojciech Mazur <wmazur@virtuslab.com>
2024-09-13 16:16:26 +0200

    test 3

--- test.scala
+++ test.scala
@@ -1,6 +1,6 @@
 val msg = s"""{"event": "foo",
              |, "ts": 123456
-             |}""".stripMargin.replaceAll("\n", System.lineSeparator())
+             |}""".stripMargin
 @main def Test = msg match {
   case s"""{"event": "$event",
 , "ts": $ts
