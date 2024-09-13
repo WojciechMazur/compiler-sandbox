2d76a685f34026bd958b7902fd1b717329816098
Wojciech Mazur <wmazur@virtuslab.com>
2024-09-13 23:56:41 +0200

    test line seperator

--- test.scala
+++ test.scala
@@ -1,9 +1,7 @@
-val msg = s"""{"event": "foo",
-             |, "ts": 123456
-             |}""".stripMargin
+val msg = s"""{"event": "foo",${System.lineSeparator()}"ts": 123456${System.lineSeparator()}}""".stripMargin
 @main def Test = msg match {
   case s"""{"event": "$event",
-, "ts": $ts
+"ts": $ts
 }""" =>
     assert(event == "foo")
     assert(ts.toInt == 123456)
