238fa9f3b4e5eb2e4603c89d15fd41e3a14de885
Wojciech Mazur <wmazur@virtuslab.com>
2024-09-14 15:24:01 +0200

    test source vs java.nio

--- test.scala
+++ test.scala
@@ -1,8 +1,21 @@
+//> using jvm 8
 val msg = s"""{"event": "foo",${System.lineSeparator()}"ts": 123456${System.lineSeparator()}}""".stripMargin
-@main def Test = msg match {
-  case s"""{"event": "$event",
+
+import java.nio.file.*
+
+@main def Test = 
+  val f = Files.createTempFile("temp", ".json")
+  Files.write(f, msg.getBytes())
+  val usingIO = new String(Files.readAllBytes(f))
+  val usingSource = scala.io.Source.fromFile(f.toFile()).mkString
+  println(s"same: ${usingIO == usingSource}")
+  List(usingIO, usingSource).zipWithIndex.foreach: (msg, idx) =>
+    println(idx -> msg.size)
+    msg match {
+    case s"""{"event": "$event",
 "ts": $ts
 }""" =>
-    assert(event == "foo")
-    assert(ts.toInt == 123456)
-}
+      assert(event == "foo")
+      assert(ts.toInt == 123456)
+  }
+
